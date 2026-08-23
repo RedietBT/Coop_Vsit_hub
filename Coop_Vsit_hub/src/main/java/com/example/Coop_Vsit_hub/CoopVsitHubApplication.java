bc197d392b26cde5b53649cdb90a67e3da019b06
unknown <redietberhanu2@gmail.com>
2026-08-23 10:23:18 +0300

    refactor: simplify main application annotations, remove test class, and collapse Swagger folders by default

--- Coop_Vsit_hub/src/main/java/com/example/Coop_Vsit_hub/CoopVsitHubApplication.java
+++ Coop_Vsit_hub/src/main/java/com/example/Coop_Vsit_hub/CoopVsitHubApplication.java
@@ -2,12 +2,8 @@ package com.example.coop_vsit_hub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com.example.coop_vsit_hub")
-@EnableJpaRepositories(basePackages = "com.example.coop_vsit_hub")
-@EntityScan(basePackages = "com.example.coop_vsit_hub")
+@SpringBootApplication
 public class CoopVsitHubApplication {
 
 	public static void main(String[] args) {
